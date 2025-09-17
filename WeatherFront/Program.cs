e3d806372bf8f2818d4a446e6ddbcd33ec6e84e6
Abhi0751 <58366593+azure0751@users.noreply.github.com>
2025-09-17 17:20:42 +0530

    Initial Code for  Front-end and Backend API
    Initial Code for  Front-end and Backend API

---
+++ WeatherFront/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace WeatherFront
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+
+            builder.Services.AddHttpClient();   // 👈 This line is missing
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapRazorPages()
+               .WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
